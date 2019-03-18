a6359d98d055a3411d5925fd2f72a8e8d41bf3b8
sindre <sindre.grostad@icloud.com>
2019-03-18 13:18:38 +0100

    added initialization vectorized operation

--- code/vectorizedOperation.jl
+++ code/vectorizedOperation.jl
@@ -1,11 +1,15 @@
 function dotsInside(x,y)
-    for i = 1:length(x)
+    n = length(x)
+    tmp1 = Vector{Float64}(undef,n)
+    for i = 1:n
         tmp1[i] = exp(x[i])
     end
-    for j = 1:length(y)
+    tmp2 = Vector{Float64}(undef,n)
+    for j = 1:n
         tmp2[j] = log(y[i])
     end
-    for k = 1:length(x)
+    tmp3 = Vector{Float64}(undef,n)
+    for k = 1:n
         tmp3[k] = tmp1[k] * tmp2[k]
     end
     return tmp3
