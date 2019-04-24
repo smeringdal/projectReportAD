47713740441a6add96ab14a3e0d8a3a30922731f
sindre <sindre.grostad@icloud.com>
2019-04-24 14:51:19 +0200

    compact revert

--- code/governingEquationsPresSolver.jl
+++ code/governingEquationsPresSolver.jl
@@ -1,3 +1,4 @@
-presEq(p, p0, dt) = (1/dt)*(pv(p)*ρ(p)-pv.(p0).*ρ.(p0))+dDiv(average(ρ(p))*flux(p))
+presEq(p,p0,dt) = (1/dt) * (pv(p)*ρ(p) - pv.(p0).*ρ.(p0)) +
+                dDiv(average(ρ(p))*flux(p))
 rateEq(p,bhp,qS) = qS - sum(q_conn(p,bhp))/rhoS
 ctrlEq(bhp) = bhp - 100*barsa
