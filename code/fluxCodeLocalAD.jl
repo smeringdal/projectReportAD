18031fe70795bf968912fb23337c06a9e7dbebaa
sindre <sindre.grostad@icloud.com>
2019-04-25 16:09:57 +0200

    mor fluxing

--- code/fluxCodeLocalAD.jl
+++ code/fluxCodeLocalAD.jl
@@ -1,4 +1,4 @@
 fluxLAD = flux(fromCell, toCell)
-fs.eqVal[from] += fluxLAD.val
-fs.globalJac[to, from] .-= fluxLAD.derivatives
-fs.globalJac[from, from] .+= fluxLAD.derivatives
+fs.eqVal[fromCell] += fluxLAD.val
+fs.globalJac[fromCell, fromCell] .+= fluxLAD.derivatives
+fs.globalJac[toCell, fromCell] .-= fluxLAD.derivatives
--- code/fluxFunctionLocalAD.jl
+++ code/fluxFunctionLocalAD.jl
@@ -1,6 +1,6 @@
 function flux(fromCell, toCell)
-    pFrom = createVariable(pressure[from], 1)
-    pTo = createVariable(pressure[to], 0)
+    pFrom = createVariable(pressure[fromCell], 1)
+    pTo = createVariable(pressure[toCell], 0)
     ρAvg = avg(rho(pFrom), rho(pTo))
     viscousFlux = -T/mu * (grad(pFrom, pTo))
     gravityFlux = T/mu * g * ρAvg * gradz
