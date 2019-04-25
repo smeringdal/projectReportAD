6dec3d09bc172136660a8c58bc71d5f5f535b33c
sindre <sindre.grostad@icloud.com>
2019-04-25 14:00:50 +0200

    assembleflowsystem update

--- code/assembleFlowSystem!.jl
+++ code/assembleFlowSystem!.jl
@@ -2,7 +2,7 @@ function assembleFlowSystem!(fs::FlowSystem, well::Well)
     resetFlowSystem!(fs)
     for fromCell = 1:numberOfCells
         for toCell in neighbours
-            if fromCell == toCell
+            if fromCell == toCell && !(fromCell in well)
                 # Add backward Euler term to FlowSystem
             elseif fromCell in well || toCell in well
                 # Add well equations to FlowSystem
