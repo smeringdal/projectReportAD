54c2f2258744ca18b65c86010ad9e4f41ca2bb81
sindre <sindre.grostad@icloud.com>
2019-04-29 09:19:56 +0200

    refactor derivativIndex name

--- code/createVariableFast.jl
+++ code/createVariableFast.jl
@@ -1,5 +1,4 @@
-function createVariable(val::Number, derivativeIndex::Int)
-    derivatives = @SVector
-                    [if i==derivativeIndex 1 else 0 end for i = 1:NUM_DERIV]
+function createVariable(val::Number, derivIx::Int)
+    derivatives = @SVector [if i==derivIx 1 else 0 end for i = 1:NUM_DERIV]
     return LAD(val, derivatives)
 end
