70bc25508764a19cde91c922e23c4be407205324
sindre <sindre.grostad@icloud.com>
2019-05-19 19:34:32 +0200

    remove dot

--- code/fluxCodeLocalAD.jl
+++ code/fluxCodeLocalAD.jl
@@ -1,4 +1,4 @@
 fluxLAD = flux(fromCell, toCell)
 fs.eqVal[fromCell] += fluxLAD.val
-fs.globalJac[fromCell, fromCell] .+= fluxLAD.derivatives[1]
-fs.globalJac[toCell, fromCell] .-= fluxLAD.derivatives[1]
+fs.globalJac[fromCell, fromCell] += fluxLAD.derivatives[1]
+fs.globalJac[toCell, fromCell] -= fluxLAD.derivatives[1]
