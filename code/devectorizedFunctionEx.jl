7ab7ee2d6d47d1e63308f0b3fb25920f3ecbfdc4
sindre <sindre.grostad@icloud.com>
2019-05-30 16:25:00 +0200

    read through locally

--- code/devectorizedFunctionEx.jl
+++ code/devectorizedFunctionEx.jl
@@ -1,7 +1,7 @@
 function devectorized(x,y)
-        h = similar(x)
-        for i = 1:length(x)
-            h[i] = exp(x[i]) * log(y[i])
-        end
-        return h
+    h = similar(x)
+    for i = 1:length(x)
+        h[i] = exp(x[i]) * log(y[i])
     end
+    return h
+end
